eb5f20fe5d350d61113be6a7e28ba359f43fd1ff
Vibhu Tripathi <amtri2030@gmail.com>
2023-10-29 16:00:45 +0530

    #82 and #83 aggreation pipeline

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -20,10 +20,10 @@ exports.getAllMovies = async (req, res) => {
     try {
 
         const features = new ApiFeatures(Movie.find(), req.query)
-                .filter()
-                .sort()
-                .limitFields()
-                .paginate();
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
 
         let movies = await features.query;
 
@@ -184,6 +184,7 @@ exports.updateMovie = async (req, res) => {
         });
     }
 }
+
 exports.deleteMovie = async (req, res) => {
     try {
         await Movie.findByIdAndDelete(req.params.id);
@@ -193,10 +194,82 @@ exports.deleteMovie = async (req, res) => {
             data: null
         });
 
-    } catch (err) {
+    } catch (error) {
         res.send(404).json({
             status: "fail",
-            message: err.message
+            message: error.message
+        });
+    }
+}
+
+exports.getMovieStats = async (req, res) => {
+    try {
+        const stats = await Movie.aggregate([
+            { $match: { ratings: { $gte: 4.5 } } },
+            {
+                $group: {
+                    // _id: null,
+                    _id: '$releaseYear',
+                    avgRating: { $avg: '$ratings' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                    priceTotal: { $sum: '$price' },
+                    movieCount: { $sum: 1 },
+                },
+            },
+            { $sort: { minPrice: -1 } },
+            { $match: { maxPrice: { $gte: 50 } } }
+        ])
+
+        res.status(200).json({
+            status: 'success',
+            count: stats.length,
+            data: {
+                stats
+            }
+        })
+
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error.message
+        });
+    }
+}
+
+exports.getMovieByGenre = async (req, res) => {
+    try {
+        const genre = req.params.genre;
+        const movies = await Movie.aggregate([
+            { $unwind: '$genres' },
+            {
+                $group: {
+                    _id: '$genres',
+                    movieCount: { $sum: 1 },
+                    movies: { $push: '$name' },
+                }
+            },
+            { $addFields: { genre: '$_id' } },
+            { $project: { _id: 0 } },
+            { $sort: { movieCount: -1 } },
+            // { $limit: 2},
+            { $match: { genre: genre } }
+
+        ])
+
+        res.status(200).json({
+            status: "success",
+            count: movies.length,
+            data: {
+                movies
+            }
+        });
+
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error.message
         });
     }
 }
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -10,6 +10,9 @@ const router = express.Router();
 
 router.route('/highest-rated').get(moviesController.getHighestRated,moviesController.getAllMovies)
 
+router.route('/movie-stats').get(moviesController.getMovieStats)
+router.route('/movie-by-genre/:genre').get(moviesController.getMovieByGenre);
+
 router.route('/')
                 .get(moviesController.getAllMovies)
                 .post(moviesController.createMovie)
