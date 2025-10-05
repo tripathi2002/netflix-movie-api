a8df48a9fd2920c23f8e6054dbc72449b3195543
vihbuTripathi <amtri2030@gmail.com>
2025-10-05 17:19:35 +0530

    adding ratelimiter

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
-// IMPORT PACKAGE 
-const express = require('express');
-const morgan = require('morgan');
-const moviesRouter = require('./routes/movies.routes');
-const authRouter = require('./routes/auth.router');
-const CustomError = require('./utils/custom.error');
-const globalErrorHandler = require('./controllers/error.controller')
+// IMPORT PACKAGE
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const moviesRouter = require("./routes/movies.routes");
+const authRouter = require("./routes/auth.router");
+const CustomError = require("./utils/custom.error");
+const globalErrorHandler = require("./controllers/error.controller");
 
 let app = express();
+
+let limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "We have received too many requests from this IP. Please try after one hour.",
+});
+
+app.use("/api", limiter);
+
 app.use(express.json());
 
-// if(process.env.NODE_ENV === 'development'){
-// }
-app.use(morgan('dev'));                 //calling morgan function
+app.use(morgan("dev")); //calling morgan function
 
-app.use(express.static('./public'));       //creating route for static folder for acces files on there
+app.use(express.static("./public")); //creating route for static folder for acces files on there
 app.use((req, res, next) => {
-    // req.requestedAt = new Date().toISOString();
-    req.requestedAt = new Date().toDateString();
-    next(); 
+  // req.requestedAt = new Date().toISOString();
+  req.requestedAt = new Date().toDateString();
+  next();
 });
 
 // app.get('/', async (req, res) => {
@@ -25,22 +34,22 @@ app.use((req, res, next) => {
 // })
 
 // USING ROUTES
-app.use('/api/v2/movies', moviesRouter);    // calling moviesRouter middleware
-app.use('/api/v2/users', authRouter);
+app.use("/api/v2/movies", moviesRouter); // calling moviesRouter middleware
+app.use("/api/v2/users", authRouter);
 
-app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: "fail",
-    //     message: `Can't find ${req.url} on the server` 
-    // });
-    // const err = new Error(`Can't find ${req.url} on the server`);
-    // err.status = 'fail';
-    // err.statusCode = 404;
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //     status: "fail",
+  //     message: `Can't find ${req.url} on the server`
+  // });
+  // const err = new Error(`Can't find ${req.url} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-    const err = new CustomError(`Can't find ${req.url} on the server`, 404);
-    next(err);  // if next(100) or any other value express will assume it an err and pass it to global error handler
+  const err = new CustomError(`Can't find ${req.url} on the server`, 404);
+  next(err); // if next(100) or any other value express will assume it an err and pass it to global error handler
 });
 
 app.use(globalErrorHandler);
-// EXPORTING APP OBJECT 
+// EXPORTING APP OBJECT
 module.exports = app;
